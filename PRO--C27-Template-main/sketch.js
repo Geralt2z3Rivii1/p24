021fe9db82eac2de9c910a6c996190de7f0c2845
Vinicius de Brito <88459597+Geralt2z3Rivii1@users.noreply.github.com>
2023-05-30 16:06:25 -0300

    Update sketch.js

--- PRO--C27-Template-main/sketch.js
+++ PRO--C27-Template-main/sketch.js
@@ -63,7 +63,7 @@ function draw() {
 
 function keyPressed() {
 	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
+		Matter.Body.applyForce(bob1.body,{x:0,y:0},{x:-50,y:-45});
 	}
 }
 
